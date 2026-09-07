b0b53821e8482526f9f525d92b141f1f9a523bc6
emmanoel <emmanoel_95@hotmail.com>
2026-09-07 19:38:49 -0300

    refactor(fraudprovider): add thread sleep to simulate slow response

--- fraud-provider/src/main/java/com/tbs/fraud/provider/service/FraudProviderService.java
+++ fraud-provider/src/main/java/com/tbs/fraud/provider/service/FraudProviderService.java
@@ -11,7 +11,20 @@ public class FraudProviderService {
     private static final BigDecimal LIMIT = new BigDecimal("5000");
 
     public FraudAnalysisResponse analyze(FraudAnalysisRequest fraudAnalysisRequest){
+        if(fraudAnalysisRequest.amount().compareTo(new BigDecimal("8888")) == 0){
+            this.simulateSlowResponse();
+        }
+
         boolean isApproved = fraudAnalysisRequest.amount().compareTo(LIMIT) <= 0;
         return new FraudAnalysisResponse(isApproved);
+    }
+
+    private void simulateSlowResponse() {
+        try {
+            Thread.sleep(10_000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new IllegalStateException("Fraud provider interrupted", e);
+        }
     }
 }
